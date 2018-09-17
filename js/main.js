ee320300757e92cd6be715030571ca24e1b47876
angizerep <maria.perez@sigis.com.ve>
2018-09-17 14:06:05 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let abrir = document.getElementById('open');
+    let menu = document.getElementById('menu');
+    abrir.addEventListener('click', apertura);
+    window.onload = function apertura(){
+        console.log('he abierto');
+    }
+    
